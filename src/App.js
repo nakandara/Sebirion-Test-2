7bb122d9593e720f69c05153809163da3732d229
Dinidu Hewage <mailtodinidu@gmail.com>
2022-09-03 09:31:42 +0530

    adding dynamic binding

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/pages/Layout';
 
-import Unauthorized from './components/pages/Unauthorized';
 import RequireAuth from './components/pages/RequireAuth';
-import AppHome from './components/pages/home/AppHome';
-import Home from './components/pages/home/Home';
-import UserRole from './components/enterp/UserRole';
-import UserRoles from './components/enterp/UserRoles';
-import Project from './components/Application/Proj/Project/Project';
-import Material from './components/Application/Manufacturing/Material/Material';
-import MaterialGroup from './components/Application/Manufacturing/MaterialGroup/MaterialGroup';
 import IsoUnit from './components/Application/AppBase/IsoUnit/IsoUnit';
 import Page404 from './components/pages/page404/Page404';
-import CoveringType from './components/Application/Invent/CoveringTypes/CoveringType';
-import Login from './Application/fndbas/Login/Login';
-import FndUser from './Application/Enterprise/User/FndUser';
-import FndUsers from './Application/Enterprise/FndUsers/FndUsers';
-import InventoryItem from './Application/Inventory/InventoryItem/InventoryItem';
-import ItemType from './Application/Inventory/ItemType/ItemType';
+
+const Home = lazy(() => import('./components/pages/home/Home'));
+const AppHome = lazy(() => import('./components/pages/home/AppHome'));
+const Unauthorized = lazy(() => import('./components/pages/Unauthorized'));
+const UserRole = lazy(() => import('./components/enterp/UserRole'));
+const UserRoles = lazy(() => import('./components/enterp/UserRoles'));
+const Project = lazy(() => import('./components/Application/Proj/Project/Project'));
+const Material = lazy(() => import('./components/Application/Manufacturing/Material/Material'));
+const MaterialGroup = lazy(() => import('./components/Application/Manufacturing/MaterialGroup/MaterialGroup'));
+const CoveringType = lazy(() => import('./components/Application/Invent/CoveringTypes/CoveringType'));
+const Login = lazy(() => import('./Application/fndbas/Login/Login'));
+const FndUser = lazy(() => import('./Application/Enterprise/User/FndUser'));
+const FndUsers = lazy(() => import('./Application/Enterprise/FndUsers/FndUsers'));
+const InventoryItem = lazy(() => import('./Application/Inventory/InventoryItem/InventoryItem'));
+const ItemType = lazy(() => import('./Application/Inventory/ItemType/ItemType'));
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Layout />} >
-        <Route path="login" element={<Login />} />
-        <Route path="unauthorized" element={<Unauthorized />} />
+        <Route path="login" element={<Suspense fallback={<>...</>}><Login /></Suspense>} />
+        <Route path="unauthorized" element={<Suspense fallback={<>...</>}><Unauthorized /></Suspense>} />
 
         <Route element={<RequireAuth allowedRoles={['ADMIN']} />}>
-          <Route path="/" element={<AppHome />}>
-            <Route path='/' element={<Home />} />
-            <Route path='user' element={<FndUser />} />
+          <Route path="/" element={<Suspense fallback={<>...</>}><AppHome /></Suspense>}>
+            <Route path='/' element={<Suspense fallback={<>...</>}><Home /></Suspense>} />
+            <Route path='inventory_item/:objId' element={<Suspense fallback={<>...</>}><InventoryItem /></Suspense>} />
+            <Route path='item_types' element={<Suspense fallback={<>...</>}><ItemType /></Suspense>} /> 
+            {/* <Route path='user' element={<FndUser />} />
             <Route path='users' element={<FndUsers />} />
             <Route path='user_role' element={<UserRole />} />
             <Route path='user_roles' element={<UserRoles />} />
@@ -39,9 +42,8 @@ function App() {
             <Route path='covering_types' element={<CoveringType />} />
             <Route path='material' element={<Material />} />
             <Route path='material_groups' element={<MaterialGroup />} />
-            <Route path='unit_measure' element={<IsoUnit />} />
-            <Route path='inventory_item/:objId' element={<InventoryItem />} />
-            <Route path='item_types' element={<ItemType />} />
+            <Route path='unit_measure' element={<IsoUnit />} />            
+            */}
           </Route>
           <Route path="*" element={<Page404 />} />
         </Route>
