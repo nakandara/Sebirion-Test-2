884a93e45d19baafcb5ae026f26cce278300c8fb
vjayathilaka <vijithakn@gmail.com>
2023-02-14 00:00:57 +0530

    assign prod url

--- src/Application/fndbas/api/axios.jsx
+++ src/Application/fndbas/api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://43.205.23.46:9090/api/";
+const BASE_URL = "http://3.6.84.137:8080/api/";
 // const BASE_URL = "http://localhost:8080/api/";
 
 export default axios.create({baseURL:BASE_URL});
